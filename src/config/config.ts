2735cb340167f8c829c29c6d5e79d12c44e4d94f
Jimmy Pan <jimmypan@ntu.edu.tw>
2020-05-03 17:28:21 -0700

    first commit

---
+++ src/config/config.ts
@@ -0,0 +1,29 @@
+export const config = {
+  "dev": {
+    "username": "udagram1",
+    "password": "udagram1",
+    "database": "udagram1",
+    "host": "udagram1.cgr3gxoshp51.us-west-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-west-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram1"
+  },
+  "dev1": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": "udagram1"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
