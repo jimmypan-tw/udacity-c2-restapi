2463500046990ee1f9e5683602b7c5b0118a4c98
Jimmy Pan <jimmypan@ntu.edu.tw>
2020-05-03 19:07:01 -0700

    updated environment variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,15 +1,15 @@
 export const config = {
+  // "dev": {
+  //   "username": "udagram1",
+  //   "password": "udagram1",
+  //   "database": "udagram1",
+  //   "host": "udagram1.cgr3gxoshp51.us-west-1.rds.amazonaws.com",
+  //   "dialect": "postgres",  
+  //   "aws_region": "us-west-1",
+  //   "aws_profile": "default",
+  //   "aws_media_bucket": "udagram1"
+  // },
   "dev": {
-    "username": "udagram1",
-    "password": "udagram1",
-    "database": "udagram1",
-    "host": "udagram1.cgr3gxoshp51.us-west-1.rds.amazonaws.com",
-    "dialect": "postgres",
-    "aws_region": "us-west-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram1"
-  },
-  "dev1": {
     "username": process.env.POSTGRESS_USERNAME,
     "password": process.env.POSTGRESS_PASSWORD,
     "database": process.env.POSTGRESS_DATABASE,
@@ -17,8 +17,7 @@ export const config = {
     "dialect": "postgres",
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
-    JWT_SECRET: 'helloworld'
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
