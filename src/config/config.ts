24d67ca3fc30f3ddda16cae6ef0b074817ac1e2d
Jimmy Pan <jimmypan@ntu.edu.tw>
2020-05-03 18:18:28 -0700

    updated aws.ts and config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -17,7 +17,8 @@ export const config = {
     "dialect": "postgres",
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": "udagram1"
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
+    JWT_SECRET: 'helloworld'
   },
   "prod": {
     "username": "",
